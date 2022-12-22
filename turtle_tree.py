051eaeb13df76cf3350b57c31a06d9efcdc194d1
Admin <damir_a_z@mail.ru>
2022-12-22 22:39:55 +0300

    First recursion program with turtle

---
+++ turtle_tree.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+
+def step(n, steps):
+    fd(steps)
+    if n == 1: bk(steps)
+    else:
+        lt(108)
+        step(n-1, steps // 2)
+        rt(72)
+        step(n-1, steps // 2)
+        rt(72)
+        step(n-1, steps // 2)
+        rt(72)
+        step(n-1, steps // 2)
+        lt(108)
+        bk(steps)
+
+
+# speed(15)   # uncomment this command and command next command
+tracer(0, 0)
+
+lt(90)
+step(6, 120)  # change 6 to 8
+
+
+mainloop()
